cf9ee081df5985198089ddfbccfa5c14dce4a82b
cassandreh1 <cassandreh01@gmail.com>
2025-02-07 11:29:05 -0500

    updating infield component styling

--- src/components/input/InputField.tsx
+++ src/components/input/InputField.tsx
@@ -1,15 +1,16 @@
 import { InputFieldProps } from "./InputField.types";
+import "./InputField.css";
 
 export function InputField({ label, type, value, onChange, placeholder }: InputFieldProps) {
     return (
-        <div className="flex flex-col gap-1">
-            <label className="text-gray-700 font-medium">{label}</label>
+        <div className="margin-bottom">
+            <label className="">{label}</label>
             <input
                 type={type}
                 value={value}
                 onChange={onChange}
                 placeholder={placeholder}
-                className="px-3 py-2 border rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
+                className="input-style"
             />
         </div>
     );
